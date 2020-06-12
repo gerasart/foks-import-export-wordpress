1990859bc91e66b9cf3759a64bbd563b267b69f4
Maksim <gerasymenkoph@gmail.com>
2020-06-13 02:51:28 +0300

    * Version: 1.8

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -91,9 +91,11 @@
   !*** ./inc/styles/main.scss ***!
   \******************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nError: No PostCSS Config found in: /Applications/MAMP/htdocs/wordpress/wp-content/plugins/foksImportExport/inc/styles\n    at /Applications/MAMP/htdocs/wordpress/wp-content/plugins/foksImportExport/node_modules/postcss-load-config/src/index.js:91:15\n    at /Applications/MAMP/htdocs/wordpress/wp-content/plugins/foksImportExport/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Applications/MAMP/htdocs/wordpress/wp-content/plugins/foksImportExport/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Applications/MAMP/htdocs/wordpress/wp-content/plugins/foksImportExport/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Applications/MAMP/htdocs/wordpress/wp-content/plugins/foksImportExport/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at /Applications/MAMP/htdocs/wordpress/wp-content/plugins/foksImportExport/node_modules/postcss-loader/src/index.js:208:9");
+// extracted by mini-css-extract-plugin
+    if(false) { var cssReload; }
+  
 
 /***/ }),
 
