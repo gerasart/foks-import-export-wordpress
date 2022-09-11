acc2d0bdabd67e2db3ce06a8c7ccb3a0949b621b
Maksim <gerasymenkoph@gmail.com>
2022-09-11 11:36:05 +0300

    fix checkout build

---
+++ inc/app/Export/ExportVariation.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Copyright (c) 2022.
+ * Created by metasync.site.
+ * Developer: gerasymenkoph@gmail.com
+ * Link: https://t.me/gerasart
+ */
+
+declare(strict_types=1);
+
+namespace Foks\Export;
+
+class ExportVariation
+{
+
+}
